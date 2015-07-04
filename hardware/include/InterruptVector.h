cee36afeb370b74f93273a739f44ca8803c67928
k-ishigaki <k-ishigaki@frontier.hokudai.ac.jp>
2015-07-04 18:15:50 +0900

    add interrupt vector declaration file(InterruptVector.c)

---
+++ hardware/include/InterruptVector.h
@@ -0,0 +1,44 @@
+/*
+ * InterruptVector.h
+ * InterruptVector is singleton instance represents the interrupt vector.
+ * Other hardware instances can use the interrupt through
+ * {@code interruptVector.addInterrupt(s, l, p)}
+ * where these arguments are explained below.
+ */
+#ifndef INTERRUPT_VECTOR_H
+#define INTERRUPT_VECTOR_H
+
+#include <stdbool.h>
+
+typedef struct {
+	/**
+	 * Returns whether interrupt pending or not.
+	 * If so, the pair listenr is called.
+	 * @return pending = true, not = false
+	 */
+	bool (*isPending)(void);
+} InterruptSource;
+
+typedef struct {
+	/**
+	 * Called when interrupt pending.
+	 * This method must be terminated shortly (about within 10 us)
+	 * because it is called at interrupt vector
+	 * and other any interrupts cannnot occur.
+	 */
+	void (*onInterrupt)(void);
+} InterruptListener;
+
+typedef struct {
+	/**
+	 * Add a interrupt source and the pair listener.
+	 * The priority is defined at Hardware.h
+	 * @param interrupt source
+	 * @param interrupt listener
+	 * @param priority
+	 */
+	void (*addInterrupt)(InterruptSource*, InterruptListener*, int);
+} InterruptVector;
+
+
+#endif /* INTERRUPT_VECTOR_H */
