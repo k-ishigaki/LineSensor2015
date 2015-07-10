c062045b50f17feab8ea991e1620762b47d7db19
k-ishigaki <k-ishigaki@frontier.hokudai.ac.jp>
2015-07-10 23:20:28 +0900

    add IO port getter to hardware

--- hardware/src/Hardware.c
+++ hardware/src/Hardware.c
@@ -4,9 +4,16 @@
 extern InterruptVector* getInterruptVector(void);
 // declared at OscillatorModule.c
 extern OscillatorModule* getOscillatorModule(void);
+// declared at IOPort.c
+extern IOPort* PortA_getInstance(void);
+extern IOPort* PortB_getInstance(void);
+extern IOPort* PortC_getInstance(void);
 
 Hardware hardware = {
 	getInterruptVector,
 	getOscillatorModule,
+	PortA_getInstance,
+	PortB_getInstance,
+	PortC_getInstance,
 };
 	
