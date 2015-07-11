eaf8e8978ace3e0f5ba3cc4b05b2154fbe35ab09
k-ishigaki <k-ishigaki@frontier.hokudai.ac.jp>
2015-07-11 22:30:57 +0900

    change method of hardware access

--- application/src/main.c
+++ application/src/main.c
@@ -1,15 +1,15 @@
 #include "Hardware.h"
 #include <stdbool.h>
 
-static IOPort* port;
+static const IOPort* port;
 
 void setup() {
-	OscillatorModule* osc = Hardware.OscillatorModule();
-	osc->InternalOscillator()->setFrequency(8000000);
-	osc->PhaseLockedLoop()->enablePLL();
+	const OscillatorModule* osc = Hardware.OscillatorModule;
+	osc->InternalOscillator->setFrequency(8000000);
+	osc->PhaseLockedLoop->enablePLL();
 	osc->selectSystemClockSource(
 			OscillatorModule_ClockSource.INTERNAL);
-	port = Hardware.PortA();
+	port = Hardware.PortA;
 	port->setDigitalOutput(0b11111111);
 }
 
