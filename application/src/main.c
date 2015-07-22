d0c7db11b24c6c763d38823dd4db27e838224d3e
k-ishigaki <k-ishigaki@frontier.hokudai.ac.jp>
2015-07-22 13:07:29 +0900

    add ADConverter module example and modify existing modules calling

--- application/src/main.c
+++ application/src/main.c
@@ -2,19 +2,38 @@
 #include <stdbool.h>
 
 static const IOPort* port;
+static const ADConverterModule* adc;
+
+int adcResult;
+
+static void onConverted() {
+	adcResult = adc->getResult();
+}
+
+InterruptListener adcListener = {
+	onConverted,
+};
 
 void setup() {
 	const OscillatorModule* osc = Hardware.OscillatorModule;
-	osc->InternalOscillator->setFrequency(8000000);
+	osc->InternalOscillator->selectFrequency(
+			InternalOscillator_Frequency.HF_8MHz);
 	osc->PhaseLockedLoop->enablePLL();
 	osc->selectSystemClockSource(
 			OscillatorModule_ClockSource.INTERNAL);
 	port = Hardware.PortA;
-	port->setDigitalOutput(0b11111111);
+	port->setPinModes(0b11111111, IOPort_PinMode.DIGITAL_OUTPUT);
+	adc = Hardware.ADConverterModule;
+	adc->enable();
+	adc->selectInputChannel(ADConverterModule_InputChannel.AN0);
+	const InterruptService* adcIntService = Hardware.ADConverterModuleInterruptService;
+	adcIntService->registerListener(&adcListener, InterruptService_Priority.HIGH_PRIORITY);
+	adcIntService->enableInterrupt();
 }
 
 void loop() {
 	port->writeDigital(0xFF);
+	adc->startConversion();
 }
 
 int main(void) {
