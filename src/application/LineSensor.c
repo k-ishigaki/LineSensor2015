d56bb239922d6ebc80509c8ac389e0bf2124dd6b
k-ishigaki <gakia5310027@gmail.com>
2015-09-28 01:07:31 +0900

    [add]line sensor hardware singleton getter

--- src/application/LineSensor.c
+++ src/application/LineSensor.c
@@ -1,5 +1,7 @@
 #include "LineSensor.h"
 
+#include <stddef.h>
+
 #include "LED.h"
 #include "PhotoTransistor.h"
 
@@ -11,28 +13,135 @@
 #ifndef COMMON_DECLARATION
 #define COMMON_DECLARATION
 
-//static const struct PhotoReflector* PhotoReflector0_getter(void);
-//static const struct PhotoReflector* PhotoReflector1_getter(void);
-//static const struct PhotoReflector* PhotoReflector2_getter(void);
-//static const struct PhotoReflector* PhotoReflector3_getter(void);
-//static const struct PhotoReflector* PhotoReflector4_getter(void);
-//static const struct PhotoReflector* PhotoReflector5_getter(void);
-//static const struct PhotoReflector* PhotoReflector6_getter(void);
-//static const struct PhotoReflector* PhotoReflector7_getter(void);
-//static const struct PhotoReflector* PhotoReflector8_getter(void);
-//static const struct PhotoReflector* PhotoReflector9_getter(void);
-//
-//const struct LineSensor LineSensor = {
-//	PhotoReflector0_getter,
-//	PhotoReflector1_getter,
-//	PhotoReflector2_getter,
-//	PhotoReflector3_getter,
-//	PhotoReflector4_getter,
-//	PhotoReflector5_getter,
-//	PhotoReflector6_getter,
-//	PhotoReflector7_getter,
-//	PhotoReflector8_getter,
-//	PhotoReflector9_getter,
-//};
-#endif /* COMMON_DECLARATION */
+// declared at DigitalPin.c
+extern const struct DigitalPin* createDigitalPin(const struct IOPort*, char, char, char);
+// declared at LED.c
+extern const struct LED* createLED(const struct DigitalPin*);
+// declared at AnalogInputPin.c
+extern const struct AnalogInputPin* createAnalogInputPin(const struct IOPort*, char, char, const struct ADConverter*, char);
+// declared at PhotoTransistor.c
+extern const struct PhotoTransistor* createPhotoTransistor(const struct AnalogInputPin*);
 
+static const struct LED* LED0_getter() {
+	static const struct LED* instance = NULL;
+	if (instance == NULL) {
+		instance = createLED(createDigitalPin(
+					Hardware.PortC(),
+					1 << 2,
+					IOPort_PinMode.DIGITAL_INPUT,
+					IOPort_PinMode.DIGITAL_OUTPUT));
+	}
+	return instance;
+}
+
+static const struct LED* LED1_getter() {
+	static const struct LED* instance = NULL;
+	if (instance == NULL) {
+		instance = createLED(createDigitalPin(
+					Hardware.PortC(),
+					1 << 3,
+					IOPort_PinMode.DIGITAL_INPUT,
+					IOPort_PinMode.DIGITAL_OUTPUT));
+	}
+	return instance;
+}
+
+static const struct LED* LED2_getter() {
+	static const struct LED* instance = NULL;
+	if (instance == NULL) {
+		instance = createLED(createDigitalPin(
+					Hardware.PortC(),
+					1 << 4,
+					IOPort_PinMode.DIGITAL_INPUT,
+					IOPort_PinMode.DIGITAL_OUTPUT));
+	}
+	return instance;
+}
+
+static const struct LED* LED3_getter() {
+	static const struct LED* instance = NULL;
+	if (instance == NULL) {
+		instance = createLED(createDigitalPin(
+					Hardware.PortC(),
+					1 << 5,
+					IOPort_PinMode.DIGITAL_INPUT,
+					IOPort_PinMode.DIGITAL_OUTPUT));
+	}
+	return instance;
+}
+
+static const struct ADConverter* getADConverterModule() {
+	static const struct ADConverter* instance = NULL;
+	if (instance == NULL) {
+		instance = Hardware.ADConverterModule(
+				ADConverterModule_PositiveReference.VDD,
+				ADConverterModule_NegativeReference.VSS,
+				ADConverterModule_ConversionClock.F_OSC_DIV_32);
+	}
+	return instance;
+}
+
+static const struct PhotoTransistor* PhotoTransistor0_getter() {
+	static const struct PhotoTransistor* instance = NULL;
+	if (instance == NULL) {
+		instance = createPhotoTransistor(createAnalogInputPin(
+					Hardware.PortA(),
+					1 << 0,
+					IOPort_PinMode.ANALOG_INPUT,
+					getADConverterModule(),
+					ADConverterModule_InputChannel.AN0));
+	}
+	return instance;
+}
+
+static const struct PhotoTransistor* PhotoTransistor1_getter() {
+	static const struct PhotoTransistor* instance = NULL;
+	if (instance == NULL) {
+		instance = createPhotoTransistor(createAnalogInputPin(
+					Hardware.PortA(),
+					1 << 1,
+					IOPort_PinMode.ANALOG_INPUT,
+					getADConverterModule(),
+					ADConverterModule_InputChannel.AN1));
+	}
+	return instance;
+}
+
+static const struct PhotoTransistor* PhotoTransistor2_getter() {
+	static const struct PhotoTransistor* instance = NULL;
+	if (instance == NULL) {
+		instance = createPhotoTransistor(createAnalogInputPin(
+					Hardware.PortA(),
+					1 << 2,
+					IOPort_PinMode.ANALOG_INPUT,
+					getADConverterModule(),
+					ADConverterModule_InputChannel.AN2));
+	}
+	return instance;
+}
+
+static const struct PhotoTransistor* PhotoTransistor3_getter() {
+	static const struct PhotoTransistor* instance = NULL;
+	if (instance == NULL) {
+		instance = createPhotoTransistor(createAnalogInputPin(
+					Hardware.PortA(),
+					1 << 4,
+					IOPort_PinMode.ANALOG_INPUT,
+					getADConverterModule(),
+					ADConverterModule_InputChannel.AN3));
+	}
+	return instance;
+}
+
+const struct LineSensor LineSensor = {
+	LED0_getter,
+	PhotoTransistor0_getter,
+	LED1_getter,
+	PhotoTransistor1_getter,
+	LED2_getter,
+	PhotoTransistor2_getter,
+	LED3_getter,
+	PhotoTransistor3_getter,
+};
+
+#endif /* COMMON_DECLARATION */
--- src/application/LineSensor.h
+++ src/application/LineSensor.h
@@ -1,17 +1,20 @@
 #ifndef LINE_SENSOR_H
 #define LINE_SENSOR_H
 
-#include "PhotoReflector.h"
+#include "LED.h"
+#include "PhotoTransistor.h"
 
 struct LineSensor {
-	const struct PhotoReflector* (*PhotoReflector0)(void);
-	const struct PhotoReflector* (*PhotoReflector1)(void);
-	const struct PhotoReflector* (*PhotoReflector2)(void);
-	const struct PhotoReflector* (*PhotoReflector3)(void);
-	const struct PhotoReflector* (*PhotoReflector4)(void);
-	const struct PhotoReflector* (*PhotoReflector5)(void);
-	const struct PhotoReflector* (*PhotoReflector6)(void);
-	const struct PhotoReflector* (*PhotoReflector7)(void);
+	const struct LED* (*getLED0)(void);    // left edge
+	const struct PhotoTransistor* (*getPhotoTransistor0)(void);
+	const struct LED* (*getLED1)(void);
+	const struct PhotoTransistor* (*getPhotoTransistor1)(void);
+	const struct LED* (*getLED2)(void);
+	const struct PhotoTransistor* (*getPhotoTransistor2)(void);
+	const struct LED* (*getLED3)(void);
+	const struct PhotoTransistor* (*getPhotoTransistor3)(void);
+	// const struct LED* (*getLED0)(void); // right edge
 };
+extern const struct LineSensor LineSensor;
 
 #endif /* LINE_SENSOR_H */
